b5e8b34cabb8f7d1d160094c1c59f7cefa03ae86
fomfom <a>
2024-12-18 18:48:27 +0900

    rename some function and variable and make multi agent ver

--- lib/giocci_engine_zenoh.ex
+++ lib/giocci_engine_zenoh.ex
@@ -12,15 +12,86 @@ defmodule GiocciEngineZenoh do
     create_session(relay_number)
   end
 
-  def create_session(0) do
-    :ok
+  @spec start_link(any(), any()) ::
+          {:ok,
+           %{
+             callback: (any() -> any()),
+             id: Relay2Engine2Relaysession,
+             subscriber: Zenohex.Subscriber.t()
+           }}
+  def start_link(relay_name, number) do
+    ## RelayからEngineを通ってRelayに返送するsubとpubをセットアップする
+    ## EngineのZenohセッションを起動
+    engine_name = System.get_env("MY_NODE_NAME")
+    {:ok, session} = Zenohex.open()
+    ## pub,subそれぞれのキーをたてる
+
+    {:ok, subscriber} =
+      Zenohex.Session.declare_subscriber(session, "from/" <> relay_name <> "/to/" <> engine_name)
+
+    {:ok, publisher} =
+      Zenohex.Session.declare_publisher(session, "from/" <> engine_name <> "/to/" <> relay_name)
+
+    id_string = "Relay2Engine2Relaysession" <> number
+    ## 状態として次の状態をもつ
+    state = %{
+      publisher: publisher,
+      subscriber: subscriber,
+      callback: &callback/2,
+      id: String.to_atom(id_string),
+      session: session
+    }
+
+    IO.inspect(String.to_atom(id_string))
+    IO.inspect("from/" <> relay_name <> "/to/" <> engine_name)
+    ## 上記の状態を保存する用のGenServerの起動
+    GenServer.start_link(__MODULE__, state, name: String.to_atom(id_string))
+    ## subの開始
+    recv_timeout(state)
+    {:ok, state}
   end
 
-  def create_session(n) do
-    number = Integer.to_string(n)
-    relay_name = System.get_env("NODE_RELAY_NAME" <> number)
-    start_link(relay_name)
-    create_session(n - 1)
+  def callback(state, message) do
+    ## Clientから送られたデータを解析して、実行する
+    %{
+      key_expr: erkey,
+      value: message_intermediate,
+      kind: kind,
+      reference: reference
+    } = message
+
+    ## msgをバイナリからlistにもどす
+    readable_msg =
+      message_intermediate
+      |> String.trim()
+      |> Base.decode64!()
+      |> :erlang.binary_to_term()
+
+    case readable_msg do
+      ## module_execの場合
+      [module, function, arity, :module_exec] = readable_msg ->
+        #  module_execする
+        module_result_reply = apply(module, function, arity)
+
+        ## 実行結果を(Relayを通して)Clientに返す
+        Zenohex.Publisher.put(
+          state.publisher,
+          (module_result_reply <> " from engine") |> :erlang.term_to_binary() |> Base.encode64()
+        )
+
+      ## module_saveの場合
+      [encode_module, :module_save] = readable_msg ->
+        ## Module_Saveを保存しロードする
+        module_save_reply = module_load_and_save({:module_save, encode_module})
+        ## ロード結果を(Relayを通して)Clientに返す
+        Zenohex.Publisher.put(
+          state.publisher,
+          module_save_reply |> :erlang.term_to_binary() |> Base.encode64()
+        )
+
+      _ = readable_msg ->
+        IO.inspect("no match")
+    end
   end
 
   def module_load_and_save({:module_save, encode_module}) do
@@ -40,7 +111,24 @@ defmodule GiocciEngineZenoh do
     Giocci.CLI.ModuleConverter.load({name, binary, path})
   end
 
-  def get_datetime() do
+  def handle_info(:loop, state) do
+    # subをループするhandle_info
+    recv_timeout(state)
+    {:noreply, state}
+  end
+
+  defp create_session(0) do
+    :ok
+  end
+
+  defp create_session(n) do
+    number = Integer.to_string(n)
+    relay_name = System.get_env("NODE_RELAY_NAME" <> number)
+    start_link(relay_name, number)
+    create_session(n - 1)
+  end
+
+  defp get_datetime() do
     {{year, month, day}, {time, min, sec}} = :calendar.local_time()
 
     datetime =
@@ -57,92 +145,6 @@ defmodule GiocciEngineZenoh do
         String.pad_leading("#{sec}", 2, "0")
 
     datetime
-  end
-
-  defp callback(state, m) do
-    ## Clientから送られたデータを解析して、実行する
-    %{
-      key_expr: erkey,
-      value: msgint,
-      kind: kind,
-      reference: reference
-    } = m
-
-    ## msgをバイナリからlistにもどす
-    msg =
-      msgint
-      |> String.trim()
-      |> Base.decode64!()
-      |> :erlang.binary_to_term()
-
-    case msg do
-      ## module_execの場合
-      [module, function, arity, :module_exec] = msg ->
-        #  module_execする
-        module_result_reply = apply(module, function, arity)
-
-        ## 実行結果を(Relayを通して)Clientに返す
-        Zenohex.Publisher.put(
-          state.publisher,
-          (module_result_reply <> " from engine") |> :erlang.term_to_binary() |> Base.encode64()
-        )
-
-      ## module_saveの場合
-      [encode_module, :module_save] = msg ->
-        ## Module_Saveを保存しロードする
-        module_save_reply = module_load_and_save({:module_save, encode_module})
-        ## ロード結果を(Relayを通して)Clientに返す
-        Zenohex.Publisher.put(
-          state.publisher,
-          module_save_reply |> :erlang.term_to_binary() |> Base.encode64()
-        )
-
-      _ = msg ->
-        IO.inspect("no match")
-    end
-  end
-
-  @spec start_link(any()) ::
-          {:ok,
-           %{
-             callback: (any() -> any()),
-             id: Relay2Engine2Relaysession,
-             subscriber: Zenohex.Subscriber.t()
-           }}
-  def start_link(relay_name) do
-    ## RelayからEngineを通ってRelayに返送するsubとpubをセットアップする
-    ## EngineのZenohセッションを起動
-    engine_name = System.get_env("MY_NODE_NAME")
-    {:ok, session} = Zenohex.open()
-    ## pub,subそれぞれのキーをたてる
-
-    {:ok, subscriber} =
-      Zenohex.Session.declare_subscriber(session, "from/" <> relay_name <> "/to/" <> engine_name)
-
-    {:ok, publisher} =
-      Zenohex.Session.declare_publisher(session, "from/" <> engine_name <> "/to/" <> relay_name)
-
-    ## 状態として次の状態をもつ
-    state = %{
-      publisher: publisher,
-      subscriber: subscriber,
-      callback: &callback/2,
-      id: Relay2Engine2Relaysession,
-      session: session
-    }
-
-    IO.inspect("from/" <> relay_name <> "/to/" <> engine_name)
-    ## 上記の状態を保存する用のGenServerの起動
-    GenServer.start_link(__MODULE__, state, name: Relay2Engine2Relaysession)
-    ## subの開始
-    recv_timeout(state)
-    {:ok, state}
-  end
-
-  def handle_info(:loop, state) do
-    # subをループするhandle_info
-    recv_timeout(state)
-    {:noreply, state}
   end
 
   defp recv_timeout(state) do
