73a527389c0059393f7fc2bac1287080de746396
s-hosoai <shintaro.hosoai@gmail.com>
2025-02-05 10:31:23 +0900

    add dotenvy library

--- mix.exs
+++ mix.exs
@@ -27,7 +27,8 @@ defmodule GiocciEngine.MixProject do
       {:cubdb, "~> 2.0.1"},
       {:giocci, "~> 0.2.1"},
       {:uniq, "~> 0.1"},
-      {:zenohex, "~>0.3.2"}
+      {:zenohex, "~>0.3.2"},
+      {:dotenvy, "~> 1.0.0"}
     ]
   end
 end
